b448248c40cf8fec9b5f833d947d183573badfb2
Nate Thompson <nthompson315@comcast.net>
2017-01-03 23:48:20 -0500

    added images and core game mechanics

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -9,17 +9,97 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var hangmanImage: UIImageView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        updateWord()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func letterButtonPressed(_ button: UIButton) {
+        let buttonTitle = button.titleLabel!.text!
+        let buttonLetter = Character(buttonTitle)
+        
+        button.backgroundColor = .darkGray
+        
+        if lastButtonPressed != nil {
+            lastButtonPressed?.backgroundColor = #colorLiteral(red: 0.7803230882, green: 0.7804364562, blue: 0.7802982926, alpha: 1)
+        }
+        lastButtonPressed = button
     }
-
+    
+    var lastButtonPressed: UIButton?
+    
+    
+    @IBAction func guessButtonPressed(_ sender: UIButton) {
+        var correct = false
+        
+        if let guess = lastButtonPressed?.titleLabel?.text {
+            for subview in wordStackView.arrangedSubviews {
+                if let label = subview as? UILabel {
+                    if label.text == guess {
+                        label.alpha = 1
+                        correct = true
+                    }
+                }
+            }
+            if correct == false {
+                wrongGuess()
+        }
+        }
+    }
+    
+    var imageNumber = 1
+    
+    func wrongGuess() {
+        lastButtonPressed?.backgroundColor = .black
+        lastButtonPressed = nil
+        
+        imageNumber += 1
+        hangmanImage.image = UIImage(named: "hangman-\(imageNumber)")
+    }
+    
+    func updateWord() {
+        let wordList = ["hangman", "apps", "carl"]
+        
+        let randomIndex = Int(arc4random_uniform(UInt32(wordList.count)))
+        var word = (wordList[randomIndex])
+        
+        //remove existing labels
+        for label in wordStackView.arrangedSubviews {
+            label.removeFromSuperview()
+        }
+        
+        for underline in underlineStackView.arrangedSubviews {
+            underline.removeFromSuperview()
+        }
+        
+        let characters = Array(word.characters)
+        for character in characters {
+            let label = UILabel()
+            label.text = String(character)
+            label.font = UIFont.systemFont(ofSize: 24)
+            label.textAlignment = .center
+            label.alpha  = 0
+            wordStackView.addArrangedSubview(label)
+            
+            label.widthAnchor.constraint(equalToConstant: 24).isActive = true
+            label.layoutIfNeeded()
+            
+            let underline = UILabel()
+            underline.text = "_"
+            underline.font = UIFont.systemFont(ofSize: 45)
+            underline.textAlignment = .center
+            underlineStackView.addArrangedSubview(underline)
+            
+            underline.widthAnchor.constraint(equalToConstant: 24).isActive = true
+            underline.layoutIfNeeded()
+        }
+    }
+    
+    @IBOutlet weak var wordStackView: UIStackView!
+ 
+    @IBOutlet weak var underlineStackView: UIStackView!
+    
 
 }
 
